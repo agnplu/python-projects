cd7041bff60be0abacf9d18658b152c7698443d7
agnplu <apludra21@gmaill.com>
2020-03-28 21:07:03 +0100

    Implemented fetching the whole document

--- webcrawler.py
+++ webcrawler.py
@@ -5,3 +5,14 @@ Created on Sat Mar 28 13:42:30 2020
 @author: Agnieszka
 """
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+print(doc)
