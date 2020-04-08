0a8316e12019ef739806996c4486038def386289
agnplu <apludra21@gmaill.com>
2020-04-08 18:49:04 +0200

    Get ol tags

--- fetch_links.py
+++ fetch_links.py
@@ -8,7 +8,17 @@ Created on Wed Apr  8 00:21:23 2020
 from bs4 import BeautifulSoup
 import urllib.request
 
+def main():
+    user_url = input("Paste your url here: \n")
+    print("Processing...") 
+    content = get_content(user_url)
+        
+    lists = content.find_all('ol')
+    return
+
 def get_content(url):
     response = urllib.request.urlopen(url)
     data = response.read()
     return BeautifulSoup(data, 'html.parser')
+
+main()
