a07a25f0369c3d22b96aa9638e39fbcc19913fc7
agnplu <apludra21@gmaill.com>
2020-04-10 17:02:35 +0200

    Fetch data from api

--- translate.py
+++ translate.py
@@ -6,10 +6,18 @@ Created on Fri Apr 10 16:13:27 2020
 """
 
 from googletrans import Translator
+import requests
 
-def main():
+def googletrans():
     translator = Translator()
     result = translator.translate('Jak się masz?')
     print(result.text)
     
-main()
+def piratetrans(text):
+    url = 'https://api.funtranslations.com/translate/pirate.json'
+    data = {'text': text}
+    
+    response = requests.post(url, data = data)
+    print(response.text)
+    
+piratetrans('Hello, sir')
